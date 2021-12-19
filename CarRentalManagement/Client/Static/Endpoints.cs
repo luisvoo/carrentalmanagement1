965d3397f6927918866461650b0a092f09a1bc36
VOO SHAO HUI, LUIS 2004888J <2004888J@student.tp.edu.sg>
2021-12-19 16:54:55 +0800

    Added Blazor List Pages with Delete function.

---
+++ CarRentalManagement/Client/Static/Endpoints.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Client.Static
+{
+    public static class Endpoints
+    {
+        private static readonly string Prefix = "api";
+
+        public static readonly string MakesEndpoint =$"{Prefix}/Makes";
+        public static readonly string ModelsEndpoint = $"{Prefix}/Models";
+        public static readonly string ColoursEndpoint=$"{Prefix}/Colours";
+        public static readonly string VehiclesEndpoint=$"{Prefix}/Vehicles";
+        public static readonly string CustomersEndpoint=$"{Prefix}/Customers";
+        public static readonly string BookingsEndpoint = $"{Prefix}/Bookings";
+
+    }
+}
